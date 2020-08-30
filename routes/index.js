63cbbe2cc3901f934cc3e9bc02417cb92d99e84a
Toshi <waykiyoharu@gmail.com>
2020-08-30 14:00:15 +0900

    selectの実装

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,15 @@ var connection = require('../mysqlConnection');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Expressdesu!' });
+  //var query = 'SELECT * FROM boards'; // ←このやり方だと「2020年08月30日 13時52分45秒」こうではなく「2020-08-30T04:53:50.000Z」こう表示される。
+  var query = 'SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM boards';
+  connection.query(query, function(err, rows) {
+    console.log(rows);
+    res.render('index', { 
+      title: 'はじめてのnode.js',
+      boardList: rows
+     });
+  });
 });
 
 router.post('/', (req,res,next)=>{
