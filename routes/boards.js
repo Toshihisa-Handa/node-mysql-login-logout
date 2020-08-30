a9f355dcf0457031dd61b92b6589747749fe077b
Toshi <waykiyoharu@gmail.com>
2020-08-30 15:18:38 +0900

    詳細ページ（boardsページ）をmysqlと接続

--- routes/boards.js
+++ routes/boards.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 var connection = require('../mysqlConnection');
 
 router.get('/:board_id', function(req, res, next) {
@@ -12,5 +13,16 @@ router.get('/:board_id', function(req, res, next) {
     });
   });
 });
+
+router.post('/:board_id',(req,res,next)=>{
+    var boardId = req.params.board_id;
+    var message = req.body.message;
+    var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+    var query = 'INSERT INTO messages (board_id, message, created_at) VALUES (?,?,?)';
+    connection.query(query,[boardId, message, createdAt],(err, rows)=>{
+        res.redirect('/boards/'+boardId);
+    });
+});
+
 
 module.exports = router;
