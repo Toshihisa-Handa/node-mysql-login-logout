791f00b4136c5ce4eab8ec0baea4131c05814423
Toshi <waykiyoharu@gmail.com>
2020-08-30 15:31:59 +0900

    詳細ページのselect実装
    <% if (messageList.length) { %>
    
    これはメッセージが1件でもある場合のみリストを表示させるような処理をしています。
    
    他にも「ログインしているユーザー」や「ブロックされている」などで場合分けが可能です。

--- routes/boards.js
+++ routes/boards.js
@@ -5,12 +5,16 @@ var connection = require('../mysqlConnection');
 
 router.get('/:board_id', function(req, res, next) {
   var boardId = req.params.board_id;
-  var query = 'SELECT * FROM boards WHERE board_id = ' + boardId;//boardIdでクリックした部分だけを取得
-  connection.query(query, function(err, board) {
-    res.render('board', {
-      title: board[0].title,//titleを渡す
-      board: board[0]//全ての結果を渡す
-    });
+  var getBoardQuery = 'SELECT * FROM boards WHERE board_id = ' + boardId;//boardIdでクリックした部分だけを取得
+  var getMessagesQuery = 'SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM messages WHERE board_id = ' + boardId;
+  connection.query(getBoardQuery, (err, board) =>{
+      connection.query(getMessagesQuery,(err, messages)=>{
+        res.render('board', {
+            title: board[0].title,//titleを渡す
+            board: board[0],//全ての結果を渡す
+            messageList:messages
+          });
+      })
   });
 });
 
