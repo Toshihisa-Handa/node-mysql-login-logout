59f3e65206c7c36b585cb29a77673394275b40f3
Toshi <waykiyoharu@gmail.com>
2020-08-31 23:13:59 +0900

    sqlに反映された

--- routes/register.js
+++ routes/register.js
@@ -9,26 +9,25 @@ router.get('/', (req,res,next)=>{
     });
 });
 
-router.post('/',(req,res,next)=>{
+router.post('/', function(req, res, next) {
     var userName = req.body.user_name;
     var email = req.body.email;
     var password = req.body.password;
     var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
-    var emailExistsQuery = 'SELECT * FROM users WHERE email = "' + email + '" LIMIT 1';//POSTされたメールアドレスと一致するユーザー情報を1件返すクエリー
-    var registerQuery = 'INSERT INTO users (user_name, email, password, created_at) VALUES(?, ?, ?, ?)';
-    connection.query(emailExistsQuery, (err,rows)=>{
-        var emailExists = email.length;
-        if(emailExists){
-            res.render('register',{
-                title:'新規会員登録',
-                emailExists:'既に登録されているメールアドレスです'
-            });
-        }else{
-            connection.query(registerQuery,[userName, email, password, createdAt],(err, rows)=>{
-                res.redirect('/login');
-            });
-        };
+    var emailExistsQuery = 'SELECT * FROM users WHERE email = "' + email + '" LIMIT 1'; // 追加
+    var registerQuery = 'INSERT INTO users (user_name, email, password, created_at) VALUES ("' + userName + '", ' + '"' + email + '", ' + '"' + password + '", ' + '"' + createdAt + '")'; // 変更
+    connection.query(emailExistsQuery, function(err, email) {
+      var emailExists = email.length;
+      if (emailExists) {
+        res.render('register', {
+          title: '新規会員登録',
+          emailExists: '既に登録されているメールアドレスです'
+        });
+      } else {
+        connection.query(registerQuery, function(err, rows) {
+          res.redirect('/login');
+        });
+      }
     });
-});
-
+  });
 module.exports = router;
