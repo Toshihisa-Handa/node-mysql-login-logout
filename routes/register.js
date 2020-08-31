5b3f32020128fd9c73162b3afe94dd9b804a9a02
Toshi <waykiyoharu@gmail.com>
2020-08-31 23:12:31 +0900

    sqlに反映されないバグになる

--- routes/register.js
+++ routes/register.js
@@ -14,7 +14,7 @@ router.post('/',(req,res,next)=>{
     var email = req.body.email;
     var password = req.body.password;
     var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
-    var emailExistsQuery = 'SELECT * FROM users WHERE email = "' + email + '" LIMIT 1';
+    var emailExistsQuery = 'SELECT * FROM users WHERE email = "' + email + '" LIMIT 1';//POSTされたメールアドレスと一致するユーザー情報を1件返すクエリー
     var registerQuery = 'INSERT INTO users (user_name, email, password, created_at) VALUES(?, ?, ?, ?)';
     connection.query(emailExistsQuery, (err,rows)=>{
         var emailExists = email.length;
